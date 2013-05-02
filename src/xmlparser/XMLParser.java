c4a683f95621898c30e1ad4aa67392d17024fb86
Denny K. Schuldt <dschuldt@espol.edu.ec>
2013-05-02 17:28:00 -0500

    Functional parser

--- src/xmlparser/XMLParser.java
+++ src/xmlparser/XMLParser.java
@@ -27,10 +27,14 @@ public class XMLParser {
     }    
     
     public static void parseXML_to_JSON() throws IOException{
-        ArrayList<String> totalEvents = new ArrayList<>();
+        
+        ArrayList<String> totalEvents = new ArrayList<>();        
         ArrayList<String> totalModules = new ArrayList<>();
         String fst_timestamp = "";
+        String currentSlide = "";
+        String lastSlide = "";
         String meeting_id = "";
+        String directory = "img-default";
         FileWriter fw = null;
         PrintWriter pw = null;
         boolean voiceLayerCreated = false;
@@ -38,9 +42,9 @@ public class XMLParser {
         boolean waiting_next = false;
         boolean writeComa = false;        
         boolean printed = false;
-        double start = 0, end = 0, saveLast = 0, last = 0;        
-        String currentSlide = "";
-        int trackEvent = 0;
+        double start = 0, end = 0, saveLast = 0, last = 0;                
+        int trackEvent = 0, cont = 0;
+        
         try{
             InputStream is = new FileInputStream("events.xml");
                 SAXReader reader = new SAXReader();
@@ -48,60 +52,14 @@ public class XMLParser {
             is.close();
             
             fw = new FileWriter("data.json");
-            pw = new PrintWriter(fw);                                    
-
-            writeJSON(pw,true); 
+            pw = new PrintWriter(fw);                                                
             
-            Element content = (Element)doc.getRootElement();            
+            Element content = (Element)doc.getRootElement();
             meeting_id = content.attribute("meeting_id").getValue();
             fst_timestamp = meeting_id.substring(findIndex(meeting_id));            
             List<Element> events = content.elements("event");
+            
             for(int i=0;i<events.size();i++){
-                //This for is just for the ParticipantTalkingEvent, so a layer will be created for this.
-                Element currentElement = events.get(i);
-                Attribute eventName = currentElement.attribute("eventname");                
-                Attribute timestamp = currentElement.attribute("timestamp");
-                
-                if(eventName.getStringValue().equals("ParticipantTalkingEvent")){
-                    //Writes on the JSON a "Text" event, which will simulate a "voice" event.
-                    if(!voiceLayerCreated){
-                        //Writes the layer
-                        pw.println("\t\t{");
-                        pw.println("\t\t\t\"name\": \"Layer\",");
-                        pw.println("\t\t\t\"id\": \"0\",");
-                        pw.println("\t\t\t\"trackEvents\": [");                       
-                        voiceLayerCreated = true;
-                        start = getSeconds(timestamp.getValue(),fst_timestamp);
-                    }else{
-                        if(currentElement.element("talking").getStringValue().equals("true")){
-                            start = getSeconds(timestamp.getValue(),fst_timestamp);
-                        }else{
-                            if(writeComa) pw.println("\t\t\t,{"); //Begin event with Coma
-                            else pw.println("\t\t\t{"); //Begin event
-                            pw.println("\t\t\t\t\"id\": \"TrackEvent"+ trackEvent +"\","); trackEvent++;
-                            pw.println("\t\t\t\t\"type\": \"text\",");
-                            pw.println("\t\t\t\t\"popcornOptions\": {");
-                            pw.println("\t\t\t\t\t\"start\":" + start + ",");
-                            pw.println("\t\t\t\t\t\"end\":" + getSeconds(timestamp.getValue(),fst_timestamp) + ",");
-                            pw.println("\t\t\t\t\t\"target\": \"video-container\",");
-                            pw.println("\t\t\t\t\t\"text\": \"Bridge -> " + currentElement.element("bridge").getStringValue()+" | Participant -> " + currentElement.element("participant").getStringValue() +"\",");
-                            pw.println("\t\t\t\t\t\"position\": \"center\",");
-                            pw.println("\t\t\t\t\t\"transition\": \"popcorn-fade\",");
-                            pw.println("\t\t\t\t\t\"fontSize\": \"12\",");
-                            pw.println("\t\t\t\t\t\"fontColor\": \"#000000\",");
-                            pw.println("\t\t\t\t\t\"left\": 10,");
-                            pw.println("\t\t\t\t\t\"top\": 10,");
-                            pw.println("\t\t\t\t},");
-                            pw.println("\t\t\t\t\"track\": \"0\",");
-                            pw.println("\t\t\t\t\"name\": \"TrackEvent3\"");                        
-                            pw.println("\t\t\t}"); //End event
-                            writeComa = true;
-                        }
-                    }
-                }
-            }pw.write("\t\t}"); writeComa = false;            
-            for(int i=0;i<events.size();i++){
-                //This for is just for the GotoSlideEvent, so a layer will be created for this.
                 Element currentElement = events.get(i);
                 Attribute eventName = currentElement.attribute("eventname");                
                 Attribute timestamp = currentElement.attribute("timestamp");
@@ -109,14 +67,32 @@ public class XMLParser {
                     last = getSeconds(timestamp.getValue(),fst_timestamp);
                 }
                 if(eventName.getStringValue().equals("GotoSlideEvent")){
+                    cont++;
+                    System.out.println("Mostrando diapositiva: " + currentElement.elements().get(0).getStringValue() + " en t = " + getSeconds(timestamp.getValue(),fst_timestamp) + " segundos.");
+                }
+            }
+                        
+            writeJSON(pw,true,last + 2.0);
+                        
+            for(int i=0;i<events.size();i++){
+                //This for is just for the GotoSlideEvent, so a layer will be created for this.
+                Element currentElement = events.get(i);
+                Attribute eventName = currentElement.attribute("eventname");                
+                Attribute timestamp = currentElement.attribute("timestamp");
+                
+                if(eventName.getStringValue().equals("GotoSlideEvent")){
                     if(!imageLayerCreated){
                         //Writes the layer
-                        pw.println("\n\t\t,{");
+                        pw.println("\t\t{");
                         pw.println("\t\t\t\"name\": \"Layer\",");
                         pw.println("\t\t\t\"id\": \"1\",");
                         pw.println("\t\t\t\"trackEvents\": [");
                         imageLayerCreated = true;
                     }
+                    if(cont == 1){
+                        saveLast = getSeconds(timestamp.getValue(),fst_timestamp);
+                        break;
+                    }                    
                     if(waiting_next){
                         end = getSeconds(timestamp.getValue(),fst_timestamp);
                         if(writeComa) pw.println("\t\t\t,{"); //Begin event with Coma
@@ -125,19 +101,19 @@ public class XMLParser {
                         pw.println("\t\t\t\t\"type\": \"image\",");
                         pw.println("\t\t\t\t\"popcornOptions\": {");
                         if(!printed){
-                            pw.println("\t\t\t\t\t\"start\": "+ start +",");
+                            pw.println("\t\t\t\t\t\"start\" : "+ start +",");
                             printed = true;
                         }else{
-                            pw.println("\t\t\t\t\t\"start\": "+ saveLast +",");
+                            pw.println("\t\t\t\t\t\"start\" : "+ saveLast +",");
                         }
-                        pw.println("\t\t\t\t\t\"end\": "+ end +",");
+                        pw.println("\t\t\t\t\t\"end\" : "+ end +",");
                         pw.println("\t\t\t\t\t\"target\": \"video-container\",");
-                        pw.println("\t\t\t\t\t\"src\": \""+ currentSlide +"\",");
+                        pw.println("\t\t\t\t\t\"src\": \""+directory+"/slide-"+(Integer.parseInt(currentSlide)+1)+".png\",");
                         pw.println("\t\t\t\t\t\"width\": 80,");
                         pw.println("\t\t\t\t\t\"height\": 80,");
-                        pw.println("\t\t\t\t\t\"top\": 0,");
-                        pw.println("\t\t\t\t\t\"left\": 0,");
-                        pw.println("\t\t\t\t\t\"transition\": \"popcorn-fade\",");
+                        pw.println("\t\t\t\t\t\"top\": 10,");
+                        pw.println("\t\t\t\t\t\"left\": 10,");
+                        pw.println("\t\t\t\t\t\"transition\": \"popcorn-fade\"");
                         pw.println("\t\t\t\t},");
                         pw.println("\t\t\t\t\"track\": \"1\",");
                         pw.println("\t\t\t\t\"name\": \"TrackEvent6\"");
@@ -145,31 +121,34 @@ public class XMLParser {
                         writeComa = true;
                         saveLast = getSeconds(timestamp.getValue(),fst_timestamp);
                         currentSlide = currentElement.elements().get(0).getStringValue();
+                        if(lastSlide.equals(currentSlide)) directory = "img";
                     }else{
                         start = getSeconds(timestamp.getValue(),fst_timestamp);
                         currentSlide = currentElement.elements().get(0).getStringValue();
+                        lastSlide = currentSlide;                        
                         waiting_next = true;
                     }                    
                 }
             }
             //Write last slide:
-            pw.println("\t\t\t,{"); //Begin event with Coma                        
+            if(cont == 1) pw.println("\t\t\t{"); //Begin event with Coma
+            else pw.println("\t\t\t,{"); //Begin event with Coma
             pw.println("\t\t\t\t\"id\": \"TrackEvent"+ trackEvent +"\","); trackEvent++;
             pw.println("\t\t\t\t\"type\": \"image\",");
             pw.println("\t\t\t\t\"popcornOptions\": {");                    
             pw.println("\t\t\t\t\t\"start\": "+ saveLast +",");                        
             pw.println("\t\t\t\t\t\"end\": "+ last +",");
             pw.println("\t\t\t\t\t\"target\": \"video-container\",");
-            pw.println("\t\t\t\t\t\"src\": \""+ currentSlide +"\",");
+            pw.println("\t\t\t\t\t\"src\": \"img/slide-"+(Integer.parseInt(currentSlide)+1)+".png\",");
             pw.println("\t\t\t\t\t\"width\": 80,");
             pw.println("\t\t\t\t\t\"height\": 80,");
-            pw.println("\t\t\t\t\t\"top\": 0,");
-            pw.println("\t\t\t\t\t\"left\": 0,");
-            pw.println("\t\t\t\t\t\"transition\": \"popcorn-fade\",");
+            pw.println("\t\t\t\t\t\"top\": 10,");
+            pw.println("\t\t\t\t\t\"left\": 10,");
+            pw.println("\t\t\t\t\t\"transition\": \"popcorn-fade\"");
             pw.println("\t\t\t\t},");
             pw.println("\t\t\t\t\"track\": \"1\",");
             pw.println("\t\t\t\t\"name\": \"TrackEvent6\"");
-            pw.println("\t\t\t}"); //End event
+            pw.println("\t\t\t}]"); //End event
             pw.write("\t\t}"); writeComa = false;
             writeJSON(pw,false);
         }catch (DocumentException | IOException e){
@@ -179,7 +158,7 @@ public class XMLParser {
         }
     }    
     
-    public static void writeJSON(PrintWriter pw, boolean init){
+    public static void writeJSON(PrintWriter pw, boolean init, Object... params){
         if(init){
             pw.println("{");
             pw.println("\t\"target\": [{");
@@ -190,13 +169,13 @@ public class XMLParser {
             pw.println("\t\"media\": [{");
             pw.println("\t\t\"id\": \"Media0\",");
             pw.println("\t\t\"name\": \"Media0\",");        
-            pw.println("\t\t\"url\": \"#t=,30\",");
+            pw.println("\t\t\"url\": [\"audio/recording.wav\"],");            
             pw.println("\t\t\"target\": \"video\",");
-            pw.println("\t\t\"duration\": 30,");
+            pw.println("\t\t\"duration\": "+params[0]+",");
             pw.println("\t\t\"controls\": false,");
             pw.println("\t\t\"tracks\": [");
         }else{
-            pw.println("],");            
+            pw.println("]");            
             pw.println("\t}],");
             pw.println("\t\"name\": \"My new project\",");
             pw.println("\t\"template\": \"basic\"");
@@ -209,7 +188,7 @@ public class XMLParser {
         return (seconds/1000.00);
     }
     
-    public static int findIndex(String s){        
+    public static int findIndex(String s){
         for(int i=0; i<s.length(); i++)
             if(s.charAt(i) == '-')
                 return (i+1);
